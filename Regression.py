eb5eb99bc4714591cc6fe545ca5ae776c7b5c8a5
Fariborz Taherkhani <ft0009@mix.wvu.edu>
2022-08-30 05:38:51 -0400

    Add files via upload

---
+++ Regression.py
@@ -0,0 +1,78 @@
+from builtins import print
+
+from numpy import load
+from numpy import expand_dims
+from numpy import asarray
+from numpy import savez_compressed
+from keras.models import load_model
+from keras.models import Sequential
+from keras.layers import Dense
+import keras as kr
+import cv2
+import pathlib
+from numpy import savez_compressed
+import numpy as np
+import os
+from PIL import Image
+def get_embedding(model, face_pixels):
+	# scale pixel values
+	face_pixels = face_pixels.astype('float32')
+	# standardize pixel values across channels (global)
+	mean, std = face_pixels.mean(), face_pixels.std()
+	face_pixels = (face_pixels - mean) / std
+	# transform face into one sample
+	samples = expand_dims(face_pixels, axis=0)
+	# make prediction to get embedding
+	yhat = model.predict(samples)
+	return yhat[0]
+
+model = load_model('model/facenet_keras.h5')
+print('Loaded Model')
+# X_train=list()
+# Y_train=list()
+# file1 = open('/media/fariborz/d35256bc-623b-42e1-b10d-48071cf615af/Arragnged/score.text', 'r')
+# Lines = file1.readlines()
+# count=0
+# for line in Lines:
+#     currentline = line.split(",")
+#     path0 = pathlib.PurePath(currentline[0])
+#     #print(path0)
+#     st='/media/fariborz/d35256bc-623b-42e1-b10d-48071cf615af/'+ str(path0)
+#     img=cv2.imread(st)
+#     img=cv2.resize(img,(160,160))
+#     embedding=get_embedding(model,img)
+#     X_train.append(embedding)
+#     Y_train.append(float(currentline[1]))
+#     count=count+1
+#     print(count)
+# X_train = asarray(X_train)
+# Y_train = asarray(Y_train)
+# savez_compressed('FaceNet_Representation.npz', X_train, Y_train)
+data = load('FaceNet_Representation.npz')
+X_train, Y_train = data['arr_0'], data['arr_1']
+model_mine = Sequential()
+model_mine.add(Dense(128, input_dim=128, kernel_initializer='normal', activation='relu'))
+model_mine.add(Dense(128, kernel_initializer='normal', activation='relu'))
+model_mine.add(Dense(128, kernel_initializer='normal', activation='relu'))
+model_mine.add(Dense(1, kernel_initializer='normal', activation="linear"))
+# Compile model
+opt = kr.optimizers.Adam(lr=1e-3, decay=1e-3 / 200)
+model_mine.compile(loss='mean_squared_error', optimizer=opt)
+# print(X_train.shape)
+# print(Y_train.shape)
+history = model_mine.fit(X_train, Y_train, epochs=1000, batch_size=1024)
+model_mine.save("model_mine.h5")
+#--------------------------------------------------------------------------------------------------
+#prediction
+# for line in Lines:
+#     currentline = line.split(",")
+#     path0 = pathlib.PurePath(currentline[0])
+#     #print(path0)
+#     st='/media/fariborz/d35256bc-623b-42e1-b10d-48071cf615af/'+ str(path0)
+#     img=cv2.imread(st)
+#     img=cv2.resize(img,(160,160))
+#     embedding=get_embedding(model,img)
+#     e=asarray([embedding])
+#     L=model_mine.predict(e)
+#     print(L)
+# stop=1
